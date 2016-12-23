4d1daec31092e54e94bf12814a9a0a9f95955df4
pedrcosta <pedrcosta@live.com.pt>
2016-12-23 11:50:45 +0000

    Created main_rf.cpp

---
+++ Animal/main_rf.cpp
@@ -0,0 +1,26 @@
+int main()
+{
+	//CThreadsAnimal animal;
+	//animal.run();
+	mqd_t mq_GPS;
+	mqd_t mq_rf;
+	mq_rf = mq_open(MQRFCOM, O_CREAT | O_EXCL, S_IRWXU | S_IRWXG, NULL);
+	mq_GPS = mq_open(MQGPS, O_CREAT | O_EXCL, S_IRWXU | S_IRWXG, NULL);
+	CRFCom teste;
+	teste.RFComPrintConf();
+	unsigned char array[32]={1,2,3,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+	//teste.RFComSender(NULL,array);
+	teste.RFComPrintRPaylo();
+	teste.RFComPrintTPaylo();
+	//teste.RFComReceiver();
+	//teste.RFComPrintRPaylo();
+	while(1)
+	{
+		teste.RFComSender(NULL,array);
+	}
+	mq_close(mq_GPS);
+	mq_close(mq_rf);
+	mq_unlink(MQGPS);
+	mq_unlink(MQRFCOM);
+
+}
